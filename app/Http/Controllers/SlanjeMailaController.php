8f374f1f9a2d72968725b6fb33d061a9ed885d21
BogMil <milanbogdanovic11@hotmail.com>
2021-02-04 00:37:46 +0100

    fix pdf

--- app/Http/Controllers/SlanjeMailaController.php
+++ app/Http/Controllers/SlanjeMailaController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Services\SlanjeMailovaService;
+use App\SlanjeMailaLog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SlanjeMailaController extends Controller
 {
@@ -19,6 +21,36 @@ class SlanjeMailaController extends Controller
         try {
             $this->_slanjeMailovaService->log($request->all());
             return $this->successfullResponse();
+        } catch (\Exception $e) {
+            return $this->systemErrorResponse($e);
+        }
+    }
+
+    public function getLog(SlanjeMailaLog $slanjeMailaLog)
+    {
+        try {
+            $json = json_decode($slanjeMailaLog->rezultat_slanja);
+            return $this->successfullResponse($json);
+        } catch (\Exception $e) {
+            return $this->systemErrorResponse($e);
+        }
+    }
+
+    public function getLogs()
+    {
+        try {
+            $logs = SlanjeMailaLog::select(
+                DB::raw('TIME(`created_at`) as created_at'),
+                'subject',
+                'uspesno',
+                'email_korisnika',
+                'id'
+            )
+                ->where('email_korisnika', auth()->user()->email)
+                ->orderBy('created_at', 'desc')
+                ->get();
+
+            return $this->successfullResponse($logs);
         } catch (\Exception $e) {
             return $this->systemErrorResponse($e);
         }
